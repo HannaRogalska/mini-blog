7fe1dc0f5ca238ef589bad8aead2b0c240fbfb4e
Hanna Rogalska <hanrogalska@gmail.com>
2025-12-03 18:38:05 +0100

    Implement connection with mongodb

---
+++ src/models/user/UserModel.ts
@@ -0,0 +1,11 @@
+import { Schema, model} from "mongoose";
+import type {IUser} from "./userType"
+
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+const User = model<IUser>("User", userSchema);
+export default User;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
 import type { PortSettings } from "./server_types";
-
+import { connectDB } from "./db/db";
 import * as cors from "cors";
-
 
 const app = express();
 dotenv.config();
@@ -11,11 +10,11 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-
 const serverPort: PortSettings = { port: Number(process.env.PORT) || 3001 };
 
 const serverRun = async () => {
   try {
+    await connectDB();
     app.listen(serverPort.port, () => {
       console.log(`✅ Server running on port ${serverPort.port}`);
     });
