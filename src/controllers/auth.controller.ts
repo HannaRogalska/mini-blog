428c1f8cfc5a2e449fdf475e5dbc23b60955f53e
Hanna Rogalska <hanrogalska@gmail.com>
2025-12-08 14:41:09 +0100

    implemented refresh route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import * as bcrypt from "bcryptjs";
 import User from "../models/user/UserModel";
 import type { Request, Response } from "express";
 import { accessToken, refreshToken } from "../services/tokenService";
+import * as jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -40,7 +41,7 @@ export const login = async (req: Request, res: Response) => {
       id: getUserFromDb._id,
       email: getUserFromDb.email,
     });
-    res.json({ accessToken: userAccessToken });
+
     const userRefreshToken = refreshToken({
       id: getUserFromDb._id,
       email: getUserFromDb.email,
@@ -50,9 +51,44 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("refreshToken", userRefreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
+      secure: false,
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
-  } catch (error) {}
+    res.json({ accessToken: userAccessToken });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+export const refreshTokenRoute = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.refreshToken;
+    if (!token) return res.status(404).json({ message: "No refresh token" });
+    const secret = process.env.REFRESH_SECRET;
+    if (!secret) return res.status(404).json({ message: "Secret not found" });
+    jwt.verify(
+      token,
+      secret,
+      async (
+        error: jwt.VerifyErrors | null,
+        decoded: string | jwt.JwtPayload | undefined
+      ) => {
+        if (error)
+          return res.status(403).json({ message: "Invalid refresh token" });
+        if (!decoded || typeof decoded === "string")
+          return res.status(403).json({ message: "Invalid token payload" });
+        const userId = decoded.id;
+        const user = await User.findById(userId);
+        if (!user || user.refreshToken !== token) {
+          return res.status(403).json({ message: "Refresh token revoked" });
+        }
+        const newAccessToken = accessToken({ userId });
+        return res.json({ newAccessToken });
+      }
+    );
+  } catch (error) {
+     return res.status(403).json({ message: "Invalid refresh token" });
+  }
 };
