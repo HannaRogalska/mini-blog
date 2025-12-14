b286e8cd72be812f7ea719c5589b519170d5662d
Hanna Rogalska <hanrogalska@gmail.com>
2025-12-14 19:27:41 +0100

    multer

--- src/controllers/post.controllers.ts
+++ src/controllers/post.controllers.ts
@@ -10,19 +10,13 @@ export const getAllPosts = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
-
 export const getPost = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     if (!id) return res.status(400).json({ message: "Id is required" });
     if (!req.user) return res.status(401).json({ message: "Unauthorized" });
-    const post = await Post.findOne({
-      _id: id,
-      author: req.user.id,
-    });
-
+    const post = await Post.findOne({ _id: id, author: req.user.id });
     if (!post) return res.status(404).json({ message: "Post not found" });
-
     return res.status(200).json(post);
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
@@ -30,7 +24,8 @@ export const getPost = async (req: Request, res: Response) => {
 };
 export const createPost = async (req: Request, res: Response) => {
   try {
-    const { title, content, image } = req.body;
+    const { title, content } = req.body;
+    console.log(content);
     if (!title || !content)
       return res
         .status(400)
@@ -38,10 +33,11 @@ export const createPost = async (req: Request, res: Response) => {
     if (!req.user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
+    const file = req.file ? req.file.filename : undefined;
     const newPost = await Post.create({
       title,
       content,
-      image,
+      image: file,
       author: req.user.id,
     });
     return res.status(201).json(newPost);
@@ -54,10 +50,7 @@ export const deletePost = async (req: Request, res: Response) => {
     if (!req.user) return res.status(401).json({ message: "Unauthorized" });
     const { id } = req.params;
     if (!id) return res.status(400).json({ message: "Id is required" });
-    const post = await Post.findOneAndDelete({
-      _id: id,
-      author: req.user.id,
-    });
+    const post = await Post.findOneAndDelete({ _id: id, author: req.user.id });
     if (!post) return res.status(404).json({ message: "Post not found" });
     return res.status(200).json({ message: "Post deleted" });
   } catch (error) {
--- src/router/postsRouter.ts
+++ src/router/postsRouter.ts
@@ -1,17 +1,43 @@
 import * as express from "express";
+import * as multer from "multer";
+import * as path from "path";
+
 import {
   getAllPosts,
   getPost,
   createPost,
-  deletePost
+  deletePost,
 } from "../controllers/post.controllers";
 import { authMiddleware } from "../middleware/authMiddleware";
 
-
-const router = express.Router()
+const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const allowedTypes = /jpeg|jpg|png|gif/;
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (allowedTypes.test(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+const upload = multer({ storage, fileFilter });
 
 router.get("/", authMiddleware, getAllPosts);
 router.get("/:id", authMiddleware, getPost);
-router.post("/", authMiddleware, createPost);
+router.post("/", authMiddleware, upload.single("file"), createPost);
 router.delete("/:id", authMiddleware, deletePost);
-export default router
+export default router;
