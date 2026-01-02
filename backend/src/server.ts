057997812888d70deefb2f04606504bb89b7745b
Hanna Rogalska <hanrogalska@gmail.com>
2026-01-02 18:32:52 +0100

    changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,12 @@ import postsRouter from "./router/postsRouter"
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", authRouter);
